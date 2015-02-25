f62ceeadc880d6fed071725453ed1282fc780680
Andreas Gounaris <andreas.gounaris@gmail.com>
2015-02-25 14:34:11 +0200

    Artist form preparation

--- RadioStation/src/radiostation/RadioStation.java
+++ RadioStation/src/radiostation/RadioStation.java
@@ -5,6 +5,8 @@
  */
 package radiostation;
 
+import radiostation.gui.ApplicationForm;
+
 /**
  *
  * @author g.giakoumis, a.gounaris, g.erenidis, t.gritsopoulos, n.doulaveras
@@ -16,7 +18,7 @@ public class RadioStation {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ArtistsForm form=new ArtistsForm();
+        ApplicationForm form=new ApplicationForm();
         form.setVisible(true);
 
     }
